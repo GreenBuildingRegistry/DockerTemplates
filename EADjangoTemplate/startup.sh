7524e21c42ec571aabcb8e81693c17e4cd69adec
Fable Turas <fable@rainsoftware.tech>
2023-05-08 11:06:14 -0700

    fixes for running with supervisor

--- EADjangoTemplate/startup.sh
+++ EADjangoTemplate/startup.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-# TODO: Add handling to better manage services. IE: gracefully quit, monitor for failures and restart, etc.
 
 # Apply Django Migrations
 cd /var/www/'{app name and/or path to backend}'
