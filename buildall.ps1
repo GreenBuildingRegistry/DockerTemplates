8bc89f1584be0b90381c875ac5a93ef8da465533
Christopher Moravec <christopher@dymaptic.com>
2023-11-09 15:55:21 -0800

    moving and renaming things

--- buildall.ps1
+++ buildall.ps1
@@ -1,4 +1,4 @@
-cd EANginxUwsgiBase
-docker build -t earthadvantage/py-base:latest -t earthadvantage/py-base:v1.0.0 -t earthadvantage/py-base:node-1.18-20.04_edge .
-cd ../EASSHBase
-docker build -t earthadvantage/py-ssh-base:latest -t earthadvantage/py-ssh-base:v1.0.0 .
+cd py-uwsgi-nginx-base
+docker build -t earthadvantage/py-uwsgi-nginx-base:latest -t earthadvantage/py-uwsgi-nginx-base:v1.0.0 .
+cd ../py-uwsgi-nginx-ssh-base
+docker build -t earthadvantage/py-uwsgi-nginx-ssh-base:latest -t earthadvantage/py-uwsgi-nginx-ssh-base:v1.0.0 .
